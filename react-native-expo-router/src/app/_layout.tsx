3bdcb73012d6fbc53009f1f60907a9890e39cfd7
shobhitjain16 <shobhit.jain@thumpn.com>
2026-05-24 01:35:55 +0530

    Implementing Default and Native Tabs

--- react-native-expo-router/src/app/_layout.tsx
+++ react-native-expo-router/src/app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
+// import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
+// export default function RootLayout() {
+//   return <Stack />;
+// }
+
+import { NativeTabs } from "expo-router/unstable-native-tabs";
+
+export default function TabLayout() {
+  return (
+    <NativeTabs>
+      <NativeTabs.Trigger name="index">
+        <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
+        <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="about">
+        <NativeTabs.Trigger.Icon sf="gear" md="settings" />
+        <NativeTabs.Trigger.Label>About</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+      <NativeTabs.Trigger name="details">
+        <NativeTabs.Trigger.Icon sf="1.magnifyingglass" md="3d_rotation" />
+        <NativeTabs.Trigger.Label>Detail</NativeTabs.Trigger.Label>
+      </NativeTabs.Trigger>
+    </NativeTabs>
+  );
 }
